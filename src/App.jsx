4ef6e01c493e73a9177443facb637c764727c59c
Kartik Sabale <61454430+kali23041@users.noreply.github.com>
2025-08-13 18:57:18 +0530

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import Landing from './pages/Landing'
 import ComponentsPage from './pages/Components'
 import DesignPrinciples from './pages/DesignPrinciples'
@@ -6,14 +6,14 @@ import AccordionPage from './pages/Accordion'
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/components" element={<ComponentsPage />} />
         <Route path="/design-principles" element={<DesignPrinciples />} />
         <Route path="/components/accordion" element={<AccordionPage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
